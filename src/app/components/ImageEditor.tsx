d87277916ee4045bb6b75c187e380b1b2a545ecf
mi1sh <106558234+mi1sh@users.noreply.github.com>
2024-04-22 09:43:47 +0500

    add color palette

--- src/app/components/ImageEditor.tsx
+++ src/app/components/ImageEditor.tsx
@@ -2,8 +2,9 @@
 
 import {Box, Button, Text} from 'usual-ui';
 import {ChangeEvent, useEffect, useState} from 'react';
-import {fabric} from 'fabric';
 import {Comfortaa} from '@next/font/google';
+import {fabric} from 'fabric';
+import { HexColorPicker } from 'react-colorful';
 
 const comfortaa = Comfortaa({
 	subsets: ['latin'],
@@ -14,20 +15,31 @@ const ImageEditor = () => {
 	const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
 	const [isImageUploaded, setIsImageUploaded] = useState(false);
 	const [scaleFactor, setScaleFactor] = useState(1);
+	const [selectedTextColor, setSelectedTextColor] = useState('#ffffff');
 
 	const img = new Image();
 
 	useEffect(() => {
-		const maxWidth = 800; // максимальная ширина изображения
-		const maxHeight = 400; // максимальная высота изображения
+		const maxWidth = 800;
+		const maxHeight = 400;
 		if (window.innerWidth <= 430) {
-			setScaleFactor(Math.min(maxWidth / img.width, maxHeight / img.height, 0.6)); // масштабируем изображение
+			setScaleFactor(Math.min(maxWidth / img.width, maxHeight / img.height, 0.6));
 		} else if (window.innerWidth <= 530) {
-			setScaleFactor(Math.min(maxWidth / img.width, maxHeight / img.height, 0.7)); // масштабируем изображение
+			setScaleFactor(Math.min(maxWidth / img.width, maxHeight / img.height, 0.7));
 		} else if (window.innerWidth <= 900) {
-			setScaleFactor(Math.min(maxWidth / img.width, maxHeight / img.height, 0.9)); // масштабируем изображение
+			setScaleFactor(Math.min(maxWidth / img.width, maxHeight / img.height, 0.9));
 		}
 	}, [img.height, img.width]);
+
+	useEffect(() => {
+		if (!canvas) return;
+		const activeObject = canvas.getActiveObject() as fabric.Textbox;
+		if (activeObject && activeObject.type === 'textbox') {
+			// @ts-ignore
+			activeObject.set('fill', selectedTextColor);
+			canvas.renderAll();
+		}
+	}, [selectedTextColor])
 
 	const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
 		setIsImageUploaded(false);
@@ -53,13 +65,25 @@ const ImageEditor = () => {
 		reader.readAsDataURL(file);
 	};
 
+	// const changeFont = () => {
+	// 	if (!canvas) return;
+	// 	const activeObject = canvas.getActiveObject() as fabric.Textbox; // Уточнение типа
+	// 	if (activeObject && activeObject.type === 'textbox') {
+	// 		activeObject.set('fontFamily', 'Impact');
+	// 		canvas.renderAll();
+	// 	}
+	// };
 
 	const addText = () => {
 		const text = new fabric.Textbox('Your text here', {
-			left: 100,
 			top: 100,
+			left: 100,
 			fontSize: 20,
-			selectable: true // Разрешаем перемещение текста
+			selectable: true,
+			fontFamily: 'Impact',
+			stroke: 'black',
+			strokeWidth: 1,
+			fill: selectedTextColor
 		});
 
 		if (!canvas) return;
@@ -107,8 +131,9 @@ const ImageEditor = () => {
 							<canvas id="canvas"
 									className="w-full h-full text-center rounded-lg drop-shadow-lg shadow-xl hover:drop-shadow-2xl hover:shadow-2xl transition"></canvas>
 						</div>
-						<div className={`${comfortaa.className} text-sm mt-3 md:text-md flex flex-row md:flex-row gap-[15vw]`}>
-							<div className={'flex flex-col'}>
+						<div
+							className={`${comfortaa.className} text-sm mt-3 md:text-md flex flex-row md:flex-row gap-[15vw]`}>
+							<div className={'flex flex-col gap-0.5'}>
 								<Button variant="underline"
 										className={'text-gray-900 w-auto mr-[0.6vw] align-left flex hover:text-gray-800'}
 										onClick={addText}>
@@ -132,17 +157,17 @@ const ImageEditor = () => {
 								<Button variant="underline"
 										className={'text-gray-900 w-auto align-left hover:text-gray-800 flex'}
 										onClick={deleteSelectedObject}>
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-										 className="w-5 h-5 mr-0.5">
-										<path fillRule="evenodd"
-											  d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
-											  clipRule="evenodd"/>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+										 strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-0.5">
+										<path strokeLinecap="round" strokeLinejoin="round"
+											  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
 									</svg>
+
 									Clear Text</Button>
 							</div>
 							<div className={'flex flex-col'}>
 								<Button variant="underline"
-										className={'text-gray-900 w-auto align-right hover:text-gray-800 flex'}
+										className={'text-gray-900 w-auto align-right hover:text-gray-800 flex mb-[1.3vh]'}
 										onClick={saveImage}>
 									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
 										 className="w-5 h-5 mr-0.5">
@@ -153,10 +178,14 @@ const ImageEditor = () => {
 									Save Image</Button>
 							</div>
 						</div>
+						<div className={'text-gray-900 w-auto flex-col flex color-picker mt-1'}>
+							<Text className={'text-gray-900 mb-2'}>Text Color:</Text>
+							<HexColorPicker color={selectedTextColor} onChange={(color) => setSelectedTextColor(color)} />
+						</div>
 					</div>
 				) : (
 					<Box
-						className="border-2 rounded-xl w-[30em] border-dashed h-auto min-h-[23vh] md:min-h-[15em] border-gray-800 items-center text-center align-middle flex justify-center">
+						className="border-2 rounded-xl w-[30em] border-dashed min-h-[25vh] md:min-h-[15em] border-gray-800 items-center text-center align-middle flex justify-center">
 						<Text className={`${comfortaa.className} text-gray-800`}>Please, upload image...</Text>
 					</Box>
 				)}
@@ -168,12 +197,13 @@ const ImageEditor = () => {
 							   className="p-5 flex flex-col w-full items-center justify-center border-2 border-gray-300 rounded-lg cursor-pointer bg-gray-50 hover:border-dashed dark:hover:bg-bray-800 dark:bg-gray-800 hover:bg-gray-100 dark:border-gray-800 dark:hover:border-gray-800 dark:hover:bg-gray-900 transition">
 							<div
 								className={`${comfortaa.className} flex flex-col items-center justify-center pt-5 pb-6`}>
-								<svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-600" aria-hidden="true"
-									 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-									<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-										  strokeWidth="2"
-										  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+									 strokeWidth={1.5} stroke="currentColor"
+									 className="w-8 h-8 text-gray-500 dark:text-gray-600 mb-2">
+									<path strokeLinecap="round" strokeLinejoin="round"
+										  d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"/>
 								</svg>
+
 								<p className="mb-2 text-sm p text-gray-500 dark:text-gray-600"><span
 									className="font-semibold">Click to upload</span> or drag and drop</p>
 								<p className="text-sm text-gray-500 dark:text-gray-600">SVG, PNG, JPG or GIF (MAX.
